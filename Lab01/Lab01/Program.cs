b85c2f2ed2b116c67830e297eb4479ceeb2d378f
Oskar <omikolaj1@gmail.com>
2018-10-23 06:39:05 -0400

    Added output Message for Lab 1

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Writes the string to the console
+            Console.WriteLine("Hello World from Oskar");
+
+            // Waits for key stroke before existing out of the console 
+            // Makes it easy to see the console output
+            Console.ReadKey();
+        }
+    }
+}
